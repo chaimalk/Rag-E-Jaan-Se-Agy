e348c339baa39f5af6a39c220422c2eda7150e2d
chaimalk <108950943+chaimalk@users.noreply.github.com>
2022-10-09 15:42:00 +0200

    Create Rag-E-Jaan-Se-Agy.ads

---
+++ Rag-E-Jaan-Se-Agy.ads
@@ -0,0 +1,55 @@
+{
+	"Playstore": {
+
+		"AdsController": {
+
+			"NetworkAds": "admob",
+
+			"BannerAdmob": "ca-app-pub-7448804041969308/9214124668",
+			"InterstitialAdmob": "ca-app-pub-7448804041969308/9721105714",
+
+			"BannerFacebook": "1541921172918417_1541921642918370",
+			"InterstitialFacebook": "1541921172918417_1541921922918342",
+			
+			"unityGameID": "4963632",
+			"UnityPlacement": "Interstitial_Android",
+
+			"StartappAds": "209433314",
+
+			"Cpa": false,
+			"ImageUrl": "https://i.imgur.com/oWEjQGy.gif",
+			"LinkCpa1": "https://cpbldi.com/911232e",
+			"LinkCpa2": "https://google.com/",
+
+			"watchNow": true,
+			"Rating": true
+
+		}
+
+	},
+	"Huaweistore": {
+
+		"AdsController": {
+
+			"NetworkAds": "startapp",
+
+			"BannerAdmob": "ca-app-pub-8082149907872048/3898325980",
+			"InterstitialAdmob": "ca-app-pub-8082149907872048/8781442418",
+
+			"BannerFacebook": "1347348165628536_1347349145628438",
+			"InterstitialFacebook": "1347348165628536_1347353132294706",
+
+			"StartappAds": "211203742",
+
+			"Cpa": false,
+			"ImageUrl": "https://i.imgur.com/oWEjQGy.gif",
+			"LinkCpa1": "https://cpbldi.com/911232e",
+			"LinkCpa2": "https://google.com/",
+
+			"watchNow": false,
+			"Rating": false
+
+		}
+
+	}
+}
